1eec8d4761215bf4037e42e40c696e0567871722
Code Sandbox <hello@codesandbox.io>
2022-02-10 22:03:26 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e55af5a566b520dec38b01973ab9db68.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8709a28f58c165ebde1ed64bcd5d40b8",
+    "url": "https://darrr1.github.io/csb-ylihe/index.html"
+  },
+  {
+    "revision": "a987a61a2d3f6b428e7a",
+    "url": "https://darrr1.github.io/csb-ylihe/static/css/main.56f1a217.chunk.css"
+  },
+  {
+    "revision": "3819d1b683b2738023da",
+    "url": "https://darrr1.github.io/csb-ylihe/static/js/2.712214da.chunk.js"
+  },
+  {
+    "revision": "a987a61a2d3f6b428e7a",
+    "url": "https://darrr1.github.io/csb-ylihe/static/js/main.cb19fe26.chunk.js"
+  },
+  {
+    "revision": "aef1848c41f50d407c00",
+    "url": "https://darrr1.github.io/csb-ylihe/static/js/runtime~main.826fb0eb.js"
+  },
+  {
+    "revision": "a48640a284521dc4184480c904702eab",
+    "url": "https://darrr1.github.io/csb-ylihe/static/media/all-programs.a48640a2.ico"
+  },
+  {
+    "revision": "199b5352639f3881890888b0df965beb",
+    "url": "https://darrr1.github.io/csb-ylihe/static/media/error.199b5352.wav"
+  },
+  {
+    "revision": "cfc00209c2e6a9df522c90e356be6241",
+    "url": "https://darrr1.github.io/csb-ylihe/static/media/find.cfc00209.svg"
+  },
+  {
+    "revision": "e23232f0ee9089a33025c8a1aa81146b",
+    "url": "https://darrr1.github.io/csb-ylihe/static/media/narrator.e23232f0.ico"
+  },
+  {
+    "revision": "bfcfd28fbabd92589d0974b2760ed758",
+    "url": "https://darrr1.github.io/csb-ylihe/static/media/restart.bfcfd28f.ico"
+  },
+  {
+    "revision": "8e8ae78a182de1b6207be2f91244f3e8",
+    "url": "https://darrr1.github.io/csb-ylihe/static/media/restore.8e8ae78a.ico"
+  },
+  {
+    "revision": "28ff52a81d3d6f9369e199a42b76a73a",
+    "url": "https://darrr1.github.io/csb-ylihe/static/media/smile.28ff52a8.svg"
+  },
+  {
+    "revision": "415b027d489dfadce00f9e229872e609",
+    "url": "https://darrr1.github.io/csb-ylihe/static/media/view-info.415b027d.ico"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://darrr1.github.io/csb-ylihe/precache-manifest.e55af5a566b520dec38b01973ab9db68.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://darrr1.github.io/csb-ylihe/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
